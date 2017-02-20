0159e99ea86134e137722f011cb50762ccd1b295
TeeCee <tc@wong.de>
2017-02-20 11:30:59 +0100

    initialize msg

--- module1/app.js
+++ module1/app.js
@@ -6,8 +6,8 @@ angular.module('LunchCheck', [])
 
 LunchCheckCtrl.$inject = ['$scope'];
 function LunchCheckCtrl($scope) {
-  $scope.boxlist = 'dd';
-  $scope.myMsg = proveList();
+  $scope.boxlist = '';
+  $scope.myMsg = '';
 
   function calcItems(string) {
   // split returns an array of Strings
@@ -15,6 +15,9 @@ function LunchCheckCtrl($scope) {
   }
 
   function proveList() {
+    if ($scope.boxlist == '') {
+      return "Please enter data first";
+    }
     if (calcItems($scope.boxlist) <= 3) {
       return "Enjoy!";
     } else {
